dd8619fecddd8ef5559ce66fa7039dcc10863908
oscargolding <oscargolding17@gmail.com>
2019-12-23 14:43:56 +1100

    Add features to do checking for posture.

--- index.js
+++ index.js
@@ -14,6 +14,36 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+  let firstVoice = newMember.voiceChannel;
+  let oldVoice = oldMember.voiceChannel;
+  // In this case we have a user that has just joined the voice channel.
+  if (firstVoice !== undefined && oldVoice === undefined) {
+      if (firstVoice.members.size === 1) {
+          // Want a timeout to happen now
+          setTimeout(handleOp, 500, firstVoice);
+      }
+  }
+});
+
+/**
+ * A function for representing a voice channel joined
+ * @param voiceChan
+ */
+function handleOp(voiceChan) {
+    // Check we don't have an undefined value
+    if (voiceChan !== undefined) {
+        // Next condition is that members must be present
+        if (voiceChan.members.size > 0) {
+            const generalChat = voiceChan.guild.channels.find(channel => channel.name === "general");
+            if (generalChat) {
+                generalChat.send('Posture check!');
+                setTimeout(handleOp, 5000, voiceChan);
+            }
+        }
+    }
+}
+
 // Need to get the super secret token for usage
 let rawData = fs.readFileSync('token.json');
 let info = JSON.parse(rawData.toString());
